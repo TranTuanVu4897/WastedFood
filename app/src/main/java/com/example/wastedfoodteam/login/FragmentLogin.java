aca8f49e619987f178afdc9268e2d8715546f3b1
tungtim2401 <62582860+tungtim2401@users.noreply.github.com>
2020-10-12 16:15:03 +0700

    Login

---
+++ app/src/main/java/com/example/wastedfoodteam/login/FragmentLogin.java
@@ -0,0 +1,104 @@
+package com.example.wastedfoodteam.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.wastedfoodteam.MainActivity;
+import com.example.wastedfoodteam.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class FragmentLogin extends Fragment {
+    GoogleSignInClient mGoogleSignInClient;
+    SignInButton signInButton;
+    int RC_SIGN_IN;
+    EditText etSDT, etPass;
+    Button btnSignIn;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        etSDT = view.findViewById(R.id.edit_sdt);
+        etPass = view.findViewById(R.id.edit_pass);
+        btnSignIn = view.findViewById(R.id.btn_signIn);
+
+        AddGoogleSignInOption();
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(etSDT.getText().equals("0385818813") && etPass.getText().equals("11011998")){
+                    Toast.makeText(getActivity(),"Dung",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(getActivity(),"Your SDT or Password wrong",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        return view;
+
+
+    }
+    private void AddGoogleSignInOption(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+    }
+    //Start Sign in Google flow
+    private void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            startActivity(new Intent(getActivity(),MainActivity.class));
+        } catch (ApiException e) {
+            e.printStackTrace();
+            Log.w("TAG","Failed code" + e.getStatusCode());
+            Log.d("Tag",e.getMessage());
+            Toast.makeText(getActivity(),"Failed Connect " + e.getStatusCode(),Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if(requestCode == RC_SIGN_IN){
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                handleSignInResult(task);
+            }
+        }catch (Exception e){
+            Log.w("SignIn","Code" + e.getStackTrace());
+        }
+    }
+
+    @Override
+    public void onStart() {
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getActivity());
+        if (account != null){
+            startActivity(new Intent(getActivity(),MainActivity.class));
+        }
+        super.onStart();
+    }
+}
