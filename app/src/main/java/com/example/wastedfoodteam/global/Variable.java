599abb8828a2072b42c7c4f4132fdd60dcffb727
notspring09 <46746920+notspring09@users.noreply.github.com>
2020-11-11 20:32:09 +0700

    variable fix

--- app/src/main/java/com/example/wastedfoodteam/global/Variable.java
+++ app/src/main/java/com/example/wastedfoodteam/global/Variable.java
@@ -6,6 +6,9 @@ import com.example.wastedfoodteam.model.Product;
 import com.example.wastedfoodteam.model.Seller;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.sql.Time;
+
+
 public class Variable {
     //public static final String ipAddress = "http://192.168.3.4/wastedfoodphp/";//Vutt ip
 //    public static final String ipAddress ="http://10.22.178.239/wastedfoodphp/";//FPT University ip
@@ -27,10 +30,10 @@ public class Variable {
     public static final String ORDER_STATUS_ORDERING = "ORDERING";
     public static final String GET_FOLLOW = "follow/getFollow.php";
     public static final String UPDATE_FOLLOW = "follow/updateFollow.php";
-    public static final String ORDER_HISTORY = "";
-    public static final String UPDATE_RATING = "";
     public static LatLng gps;
     public static Account ACCOUNT;
+    public static final String UPDATE_RATING = "order/buyerUpdateRating.php";
+    public static final String ORDER_HISTORY = "order/buyerOrderHistory.php";
 
     public static Seller SELLER;
     public static Buyer buyer;
@@ -40,4 +43,6 @@ public class Variable {
     public static int CHECK_LOGIN;
 
     public static final String noImageUrl = "https://www.actbus.net/fleetwiki/images/8/84/Noimage.jpg";
+    public static Time startTime = Time.valueOf("11:00:00"), endTime = Time.valueOf("22:00:00");
+    public static String distance = "20", discount = "90";
 }
