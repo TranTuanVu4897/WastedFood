6910739ce1d5ea4fcc28c92e806253d495eed526
notspring09 <46746920+notspring09@users.noreply.github.com>
2020-12-09 17:41:29 +0700

    Fix UI

--- app/src/main/java/com/example/wastedfoodteam/seller/notification/NotificationFragment.java
+++ app/src/main/java/com/example/wastedfoodteam/seller/notification/NotificationFragment.java
@@ -22,6 +22,8 @@ import com.android.volley.toolbox.Volley;
 import com.example.wastedfoodteam.R;
 import com.example.wastedfoodteam.global.Variable;
 import com.example.wastedfoodteam.model.Notification;
+import com.example.wastedfoodteam.seller.order.ProductOrderSellerFragment;
+import com.example.wastedfoodteam.seller.order.SellerOrderDetailFragment;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
