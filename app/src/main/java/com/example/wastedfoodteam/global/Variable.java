e7b84ef4aaf9ebe47c0e6c000a6b60354b76bfa3
notspring09 <46746920+notspring09@users.noreply.github.com>
2020-12-14 15:25:16 +0700

    fix register

--- app/src/main/java/com/example/wastedfoodteam/global/Variable.java
+++ app/src/main/java/com/example/wastedfoodteam/global/Variable.java
@@ -12,10 +12,10 @@ import java.sql.Time;
 
 
 public class Variable {
-    public static final String IP_ADDRESS = "http://192.168.3.4/wastedfoodphp/";//Vutt ip
+    //public static final String IP_ADDRESS = "http://192.168.3.4/wastedfoodphp/";//Vutt ip
 //    public static final String IP_ADDRESS = "http://10.22.178.239/wastedfoodphp/";//FPT University ip
 //    public static final String IP_ADDRESS = "http://192.168.1.19/wastedfoodphp/";//TungPT ip
-//    public static final String IP_ADDRESS = "http://192.168.1.10/wastedfoodphp/";//DucHC ip
+    public static final String IP_ADDRESS = "http://192.168.1.253/wastedfoodphp/";//DucHC ip
 //    public static final String IP_ADDRESS ="http://192.168.156.2/wastedfoodphp/";//DucHC ip lan
 //    public static final String IP_ADDRESS ="http://192.168.1.29/wastedfoodphp/";//DucHC ip lan
 //    public static final String IP_ADDRESS ="https://wasted-food-service.herokuapp.com/";//heroku server
